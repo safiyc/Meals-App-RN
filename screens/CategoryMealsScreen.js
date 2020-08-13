1ce3de6bee9208652982a9451b9a8b067bf852b4
Safiy Cham <csafiy@yahoo.com>
2020-08-13 12:02:48 -0700

    create meals model, dummy data and display

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealScreen = props => {
+  const renderMealItem = itemdData => {
+    return (<View><Text>{itemdData.item.title}</Text></View>);
+  };
+
   const catId = props.navigation.getParam('categoryId');
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  // const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem} />
+
+      {/* <Text>The Category Meal Screen</Text>
       <Text>{selectedCategory.title}</Text>
       <Button title='Go to Meal Detail!' onPress={() => {
         props.navigation.navigate('MealDetail');
-      }} />
+      }} /> */}
       {/* <Button title='Go to same page with new content' onPress={() => {
         props.navigation.push('MealDetail');
       }} /> */}
