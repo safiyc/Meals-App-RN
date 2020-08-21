729ea856ef0cb32c6bea75ac2285f484f0641495
Safiy Cham <csafiy@yahoo.com>
2020-08-21 09:19:19 -0700

    Add styling to drawer and tab navigations

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -18,6 +18,12 @@ const defaultStackNavOptions = {
   headerTitle: 'This placeholder title is replaced when specified',
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {  // only for ios, not android
+    fontFamily: 'open-sans'
   },
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
@@ -43,7 +49,7 @@ const FavNavigator = createStackNavigator({
   MealDetail: MealDetailScreen
 }, {
   defaultNavigationOptions: defaultStackNavOptions
-})
+});
 
 const tabScreenConfig = {
   Meals: {
@@ -52,7 +58,8 @@ const tabScreenConfig = {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.primaryColor  // tabBarColor is for android and shifting set to true
+      tabBarColor: Colors.primaryColor,  // tabBarColor is for android and shifting set to true
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text> : Meals  // tabBarlabel bc android doesnt have labelStyle
     }
   },
   Favorites: {
@@ -62,7 +69,8 @@ const tabScreenConfig = {
       tabBarIcon: (tabInfo) => { // tabInfo from tabBarOptions
         return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.accentColor
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text> : Favorites  // tabBarlabel bc android doesnt have labelStyle
     }
   }
 };
@@ -71,21 +79,47 @@ const MealsFavTabNavigator =
   Platform.OS === 'android'
     ? createMaterialBottomTabNavigator(tabScreenConfig, {
       activeColor: 'white',
-      shifting: true
+      shifting: true,
+      barStyle: {
+        backgroundColor: Colors.primaryColor
+      }
     })
     : createBottomTabNavigator(tabScreenConfig, {
       tabBarOptions: {
+        labelStyle: {
+          fontFamily: 'open-sans-bold'
+        },
         activeTintColor: Colors.accentColor
       }
     });
 
 const FiltersNavigator = createStackNavigator({
   Filters: FiltersScreen
+}, {
+  // navigationOptions: {
+  //   drawerLabel: 'Filters!'
+  // },
+  defaultNavigationOptions: defaultStackNavOptions
 });
 
 const MainNavigator = createDrawerNavigator({
-  MealsFavs: MealsFavTabNavigator,
+  // MealsFavs: MealsFavTabNavigator,
+  MealsFavs: {
+    screen: MealsFavTabNavigator,
+    navigationOptions: {
+      drawerLabel: 'Meals'
+    }
+  },
   Filters: FiltersNavigator
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: 'open-sans-bold',
+      // fontSize: 30
+    }
+  },
+  // drawerWidth: '50%'
 });
 
 // export default createAppContainer(MealsNavigator);
